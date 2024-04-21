d9de5b6632c913ee07a050246e527a7687169599
leblanc-i <idrissaamadouousmane@gmail.com>
2024-04-21 23:46:13 +0100

    Version finale

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let gamePlaying = false; // Si on est pas entrain de jouer
 const gravity = .5; // La gravité de l'oiseau
 const speed = 6.2; // La vitesse du jeu
 const size = [51, 36]; // La taille de l'oiseau largeur et hauteur
-const jump = -11.5; // Le saut de l'oiseau
+const jump = -10; // Le saut de l'oiseau
 const cTenth = (canvas.width / 10); // 1/10 de la largeur du canvas
 
 // Parametre des poteaux
@@ -52,10 +52,48 @@ const render = () => {
         flyHeight = ((canvas.height / 2) - (size[1] / 2));
         
         // Ecrire dans le canvas 
-        ctx.fillText(`Meilleur score : ${bestScore}`, 55, 245);
-        ctx.fillText(`Cliquez pour jouer`, 48, 535);
+        ctx.fillText(`Meilleur Score : ${bestScore}`, 55, 245);
+        ctx.fillText(`Cliquez pour jouer`, 48, 450);
+        ctx.fillText(`Appuyer sur l'ecran`, 48, 510);
+        ctx.fillText(`Ou`, 200, 540);
+        ctx.fillText(`La touche ESPACE`, 60, 570);
+        ctx.fillText(`Pour Sauter`, 120, 600);
         ctx.font = "bold 30px courier"
     }
+
+    // L'affichage des poteaux
+    if (gamePlaying) {
+        // On prend nos trois poteaux qu'on a générer et on les map
+        pipes.map(pipe => {
+            pipe[0] -= speed;
+
+            // On dessine ensuite nos poteaux
+            // Poteaux d'en haut
+            ctx.drawImage(img, 432, 588 - pipe[1], pipeWidth, pipe[1], pipe[0], 0, pipeWidth, pipe[1]);
+
+            // Poteaux d'en bas
+            ctx.drawImage(img, 432 + pipeWidth, 108, pipeWidth, canvas.height - pipe[1] + pipeGap, pipe[0], pipe[1] + pipeGap, pipeWidth, canvas.height - pipe[1] + pipeGap);
+
+            if (pipe[0] <= -pipeWidth) {
+                currentScore ++;
+                bestScore = Math.max(bestScore, currentScore);
+                // Supprimer le poteaux qui passe et ajouter un poteaux à la fin 
+                pipes = [...pipes.slice(1), [pipes[pipes.length - 1][0] + pipeGap + pipeWidth, pipeLoc()]];
+            }
+            // La fin de la partie quand on tape un poteaux
+            if ([
+                pipe[0] <= cTenth + size[0],
+                pipe[0] + pipeWidth >= cTenth,
+                pipe[1] > flyHeight || pipe[1] + pipeGap < flyHeight + size[1]
+            ].every(elem => elem)) {
+                gamePlaying = false;
+                setup();
+            }
+        })
+    }
+    // Affichage du Score 
+    document.getElementById("bestScore").innerHTML = `Meilleur : ${bestScore}`;
+    document.getElementById("currentScore").innerHTML = `Score : ${currentScore}`;
 
     // Et on le relance à chaque fois
     window.requestAnimationFrame(render);
@@ -64,3 +102,9 @@ setup();
 img.onload = render; // Au chargement de l'image tu peut commencer à lancer le render
 document.addEventListener("click", () => gamePlaying = true);
 window.onclick = () => flight = jump;
+window.onkeydown = (e) => {
+    if (e.keyCode === 32) {
+        flight = jump
+    }
+};
+document.body.style.zoom = "80%";
